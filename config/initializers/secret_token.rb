2fce5cf45e7180b5a3f5b9461154ac9808a827a8
Andrew de Laix <delaix@mac.com>
2012-09-18 18:56:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Okcmadison::Application.config.secret_token = '06cf5622a43fad059af5317012195548c76188e151820e408988f489c89c66f2784999094d11500018d751a399f6a9cca21f8532b80c4800d8b21795b778fe38'
