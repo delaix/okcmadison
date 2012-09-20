074edd765fa808c297eb3aab6b3bc04dd47f2331
Andrew de Laix <delaix@mac.com>
2012-09-19 19:37:30 -0500

    generated first static pages II: home, about, and classes.

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'classes'" do
+    it "returns http success" do
+      get 'classes'
+      response.should be_success
+    end
+  end
+
+end
